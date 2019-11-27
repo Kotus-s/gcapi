e831f1348f733505d1b269959c000796bfc98425
Bastien Deshayes <bastien35@gmail.com>
2019-11-27 23:20:18 +0100

    :white_check_mark: Adding URI test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,20 @@
 const assert = require('assert');
 const GCApi = require('../');
 
+const hostname = 'dev-api.g-ca.fr';
+
 describe('GCApi', () => {
   it('should throw an error when no api key was provided', () => {
-    assert.throws(() => new GCApi({}), 'Api key is required.');
+    assert.throws(() => new GCApi({}));
+  });
+
+  const app = new GCApi({
+    host: hostname,
+    apiKey: 'API_KEY_HERE',
+  });
+
+  it('default uri should be well formed', () => {
+    const uri = app.makeUri({ pathname: '/path/test' });
+    assert.equal(uri, `https://${hostname}/v1/path/test`);
   });
 });
